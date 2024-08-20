b3f69ee6cba9f25b6180e2badee79f1d7e32764d
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2024-08-21 00:25:24 +0200

    Bottom template

--- src/scripts/searchButton.js
+++ src/scripts/searchButton.js
@@ -1,24 +1,32 @@
 let searchIcon = document.querySelector(".search-icon");
 let input = document.querySelector("#city");
+let errorBox = document.querySelector(".error-box");
 
 searchIcon.addEventListener("click", () => {
     if (input.value != "") {
         fetchCityData(input.value).then(function (result) {
             if (result != undefined) {
-                //city is ok
+                //city is ok, continue
+                console.log(result);
+                input.value = "";
             } else {
-                //city is nok
+                //city input is invalid, open box
+                errorBox.style.display = "flex";
+                //disable keyboard
+                document.onkeydown = function (e) {
+                    return false;
+                };
             }
         });
     }
 });
-
+//fetches data from visual crossing for last 15 days
 async function fetchCityData(cityName) {
     try {
         const data = await fetch(
             "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
                 cityName +
-                "?key=43Q4WDFV8SE7G2BPQ3LVEP438",
+                "?key=43Q4WDFV8SE7G2BPQ3LVEP438&unitGroup=metric",
             { mode: "cors" }
         );
         const data2 = await data.json();
@@ -27,3 +35,14 @@ async function fetchCityData(cityName) {
         console.log(error);
     }
 }
+
+//closing box for wrong input
+let closeButton = document.querySelector(".close-button");
+closeButton.addEventListener("click", () => {
+    errorBox.style.display = "none";
+
+    //enable keyboard
+    document.onkeydown = function (e) {
+        return true;
+    };
+});
