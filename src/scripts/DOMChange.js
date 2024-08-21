bf3364acdf3ff5f38875d442c702a4272de9c774
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2024-08-22 00:46:38 +0200

    faze mjeseaca

--- src/scripts/DOMChange.js
+++ src/scripts/DOMChange.js
@@ -1,6 +1,8 @@
 let box1 = document.querySelectorAll(".box-1");
 let maxTemp = document.querySelectorAll(".up-text");
 let minTemp = document.querySelectorAll(".down-text");
+let moonShape = document.querySelectorAll(".moon-shape");
+
 export function DOMChange(data) {
     console.log(data);
     for (let i = 1; i < 8; i++) {
@@ -32,5 +34,42 @@ export function DOMChange(data) {
 
         maxTemp[i - 1].innerHTML = data.days[i].tempmax + " °C";
         minTemp[i - 1].innerHTML = data.days[i].tempmin + " °C";
+
+        let moonChange = data.days[i].moonphase;
+        console.log(moonChange);
+        switch (true) {
+            case moonChange == 0:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-new.svg";
+                break;
+            case moonChange < 0.25:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-waxing-crescent.svg";
+                break;
+            case moonChange == 0.25:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-first-quarter.svg";
+                break;
+            case moonChange < 0.5:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-waxing-gibbous.svg";
+                break;
+            case moonChange == 0.5:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-full.svg";
+                break;
+            case moonChange < 0.75:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-waning-gibbous.svg";
+                break;
+            case moonChange == 0.75:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-last-quarter.svg";
+                break;
+            case moonChange < 1:
+                moonShape[i - 1].src =
+                    "../images/icons/moon-shapes/moon-waning-crescent.svg";
+                break;
+        }
     }
 }
